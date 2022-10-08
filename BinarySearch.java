d9c3dcead630bd9cd08f5c0de802bc62c35a023a
AnkitKumar989 <91775514+AnkitKumar989@users.noreply.github.com>
2022-10-08 15:43:56 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+		int arr[]= {5,7,10,12,15,20,27,31,36,42,55,58,60,65,70,80};
+		int item=65;
+		System.out.println(binarySearch(arr, item));
+	}
+	public static int binarySearch(int [] arr,int item) {
+		int low=0;
+		int high=arr.length-1;
+		
+		
+		while(low<high) {
+			int mid=(low+high)/2;
+			if(arr[mid]<item) {
+				low=mid+1;
+			}
+			else if(arr[mid]>item) {
+				high=mid-1;
+			}
+			else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+}
---
+++ BubbleSort.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int arr[]= {8,65,565,434,2};
+		System.out.println(Arrays.toString(sorting(arr)));
+		
+	}
+	public static int[] sorting(int[] arr) {
+		
+		for(int counter=0;counter<arr.length-1;counter++)
+		{ 
+
+			for(int j=0;j<arr.length-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					swap(arr, j, j+1);
+					
+				}
+				
+			}
+			
+			System.out.println("counter"+counter+" : "+Arrays.toString(arr));
+
+		}
+		return arr;
+		
+	}
+	public static void swap(int arr[],int i,int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+		
+	}
+
+}
